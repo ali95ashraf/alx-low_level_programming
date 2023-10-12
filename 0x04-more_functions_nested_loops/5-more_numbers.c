6ec60b2cb413b47a852893f7c67375a5ca1f6b3c
ali95ashraf <aliashrafsaeed@gmail.com>
2023-10-12 16:59:29 -0700

    Your commit message

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,21 +9,18 @@
 
 void more_numbers(void)
 {
-	int num, row, count;
+	int i, num;
 
-	for (row = 1; row < 10; row++)
+	for (i = 0; i < 10; i++)
 	{
-		for (count = 0; count <= 14; count++)
+		for (num = 0; num <= 14; num++)
 		{
-			num = count;
-			if (count > 9)
+			if (num > 9)
 			{
-				_putchar(1 + 48);
-				num = count % 10;
+				_putchar('0' + num / 10);
 			}
-			_putchar(num + 48);
+			_putchar('0' + num % 10);
 		}
 		_putchar('\n');
 	}
 }
-
