53e925513917e99131eeabcd81b3607eb7da9fd3
ali95ashraf <aliashrafsaeed@gmail.com>
2023-10-12 13:31:54 -0700

    2. 0 > 972?

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -10,12 +10,16 @@ int main(void)
 	int i;
 
 	printf("Infinite loop incoming :(\n");
+
 	i = 0;
 
-       /* while (i < 10)*/
-       /* {*/
-       /*         putchar(i);*/
-       /*}*/
+	/**
+	* while (i < 10)
+	* {
+	*     putchar(i);
+	*     i++; // Increment i to avoid an infinite loop
+	* }
+	*/
 
 	printf("Infinite loop avoided! \\o/\n");
 
