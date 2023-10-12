91cf06ea0dd7e63def5a8d7a2e2d65507af70529
ali95ashraf <aliashrafsaeed@gmail.com>
2023-10-12 14:40:36 -0700

    1. Like, comment, subscribe

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,15 +13,11 @@ int main(void)
 
 	i = 0;
 
-	/**
-	* while (i < 10)
-	* {
-	*     putchar(i);
-	*     i++; // Increment i to avoid an infinite loop
-	* }
-	*/
+	/* while (i < 10)*/
+	/*{*/
+	/*         putchar(i);*/
+	/*}*/
 
 	printf("Infinite loop avoided! \\o/\n");
-
 	return (0);
 }
