90fce465aeca4263d3d5e92b9d9ec2e789c55f50
ali95ashraf <aliashrafsaeed@gmail.com>
2023-10-16 15:57:12 -0700

    fix2

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -14,11 +14,13 @@ void print_rev(char *s)
 	int i = 0;
 
 	while (s[i])
-		i++;
+		i--;
 
-	while (i--)
+	while (i >= 0)
 	{
-		_putchar(s[i]);
+		putchar(s[i]);
+		i--;
 	}
-	_putchar('\n');
+	putchar('\n');
 }
+
