511a0b693fb8d14c9bee6606f43c147f12b364a6
ali95ashraf <aliashrafsaeed@gmail.com>
2023-10-16 18:34:16 -0700

    10

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * _atoi - Converts a string to an integer.
+ * @s: The string to be converted.
+ *
+ * Return: The integer value.
+ */
+
+int _atoi(char *s)
+{
+	int result = 0;
+	int sign = 1;
+	int i = 0;
+
+	while (s[i] == ' ')
+		i++;
+
+	if (s[i] == '-')
+	{
+		sign = -1;
+		i++;
+	}
+	else if (s[i] == '+')
+	{
+		i++;
+	}
+
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+		if (result > (INT_MAX - (s[i] - '0')) / 10)
+		{
+			return (sign == 1) ? INT_MAX : INT_MIN;
+		}
+		result = result * 10 + (s[i] - '0');
+		i++;
+	}
+
+	return result * sign;
+}
+
