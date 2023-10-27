11c6eec1aa419870e4cdd80eb32fccb7e788c584
ali95ashraf <aliashrafsaeed@gmail.com>
2023-10-27 16:32:12 -0700

    0x0A

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,14 +2,15 @@
 
 /**
  * main - Entry point
- * 
  * Description: A program that prints its name.
- * 
+ * @argc: int
+ * @argv: list
  * Return: Always 0 (Success)
  */
 
-int main(int argc, char *argv[])
+int main(int argc, char const *argv[])
 {
+	(void)argc;
 	printf("%s\n", argv[0]);
 	return (0);
 }
