c2dec43bf5f5a884eefb80636a3d973aee936736
ali95ashraf <aliashrafsaeed@gmail.com>
2023-11-09 14:01:51 -0800

    fix

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -53,20 +53,20 @@ void format_string(char *separator, va_list ap)
 void print_all(const char *const format, ...)
 {
 	int i = 0, j;
-    	char *separator = "";
-    	va_list ap;
-    	token_t tokens[] = {
-        	{"c", format_char},
-        	{"i", format_int},
-        	{"f", format_float},
-       	 	{"s", format_string},
-        	{NULL, NULL}
-    	};
+	char *separator = "";
+	va_list ap;
+	token_t tokens[] = {
+		{"c", format_char},
+		{"i", format_int},
+		{"f", format_float},
+		{"s", format_string},
+		{NULL, NULL}
+	};
 
 	va_start(ap, format);
-    	while (format && format[i])
+	while (format && format[i])
 	{
-        	j = 0;
+		j = 0;
 		while (tokens[j].token)
 		{
 			if (format[i] == tokens[j].token[0])
